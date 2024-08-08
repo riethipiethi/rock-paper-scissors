ce75e65a1d66271125b90292ed849f689d38ec6f
Felix Rieth <felixrieth@e.email>
2024-08-08 09:45:21 +0200

    Implement getComputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 /* CLI version of the famous game 'Rock Paper Scissors'*/
+
+function getComputerChoice() {
+    let randomNumber = Math.random();
+    if (randomNumber < 1 / 3) {
+        return "rock";
+    } else if (randomNumber < 2 / 3) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+let computerChoice = getComputerChoice();
