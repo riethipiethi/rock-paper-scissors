fcefafe7c33a3be57c512c421fd9b2838f16b6b3
Felix Rieth <felixrieth@e.email>
2024-08-08 10:05:51 +0200

    Implement getHumanChoice() function

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 /* CLI version of the famous game 'Rock Paper Scissors'*/
 
+const POSSIBLE_CHOICES = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
     let randomNumber = Math.random();
     if (randomNumber < 1 / 3) {
-        return "rock";
+        return POSSIBLE_CHOICES[0];
     } else if (randomNumber < 2 / 3) {
-        return "paper";
+        return POSSIBLE_CHOICES[1];
     } else {
-        return "scissors";
+        return POSSIBLE_CHOICES[2];
     }
 }
 
+function getHumanChoice() {
+    let choice = "";
+    do {
+        choice = prompt("What do you choose?");
+        if (POSSIBLE_CHOICES.indexOf(choice) === -1) {
+            alert(
+                `You have to choose between "${POSSIBLE_CHOICES[0]}", "${POSSIBLE_CHOICES[1]}" and "${POSSIBLE_CHOICES[2]}".`
+            );
+        }
+    } while (POSSIBLE_CHOICES.indexOf(choice) === -1);
+    return choice;
+}
+
+let humanChoice = getHumanChoice();
 let computerChoice = getComputerChoice();
+
+console.log(humanChoice);
+console.log(computerChoice);
